07f5447d8b97b223ab47859caebbec75562d44e1
Christopher Kim <chris.kim1990@gmail.com>
2021-02-08 23:07:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,6 +25,7 @@ end
 def using_delete(instructors, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   instructors.delete("Steven")
+  instructors
 end
 
 def using_delete_at(famous_robots, string)
