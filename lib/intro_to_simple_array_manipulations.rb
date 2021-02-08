c814e5b7685bd57423ce9db6518d82075f04f808
Christopher Kim <chris.kim1990@gmail.com>
2021-02-08 22:36:47 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,12 @@
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  my_favorite_things.concat(more_favs)
+  my_favorite_things
+end
+
+def using_insert(list_of_programming, another_language)
+  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  another_language = "Python"
+  list_of_programming_languages.insert(another_language)
+end
